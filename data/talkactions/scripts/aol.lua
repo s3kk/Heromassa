1e6f64113708a27cfb0072e9cd263ee45cbf8d59
Sekk <kicknclash@gmail.com>
2016-10-12 17:06:23 -0300

    .

--- data/talkactions/scripts/aol.lua
+++ data/talkactions/scripts/aol.lua
@@ -1,10 +1,51 @@
+local config = {
+
+	price = 10000, -- preço unidade
+
+	effect = CONST_ME_POFF, -- efeito ao comprar aol
+
+	id = 2173 -- id do item comprado (aol no caso)
+
+}
+
+
 function onSay(cid, words, param)
-if doPlayerRemoveItem(cid,2160,1) then
-doPlayerAddItem(cid,2173,1)
-doSendMagicEffect(getPlayerPosition(cid), CONST_ME_MORTAREA)
-doCreatureSay(cid, "!aol", TALKTYPE_ORANGE_1)
-else
-doPlayerSendCancel(cid, 'You do not have 1 crystal coin.')
-doSendMagicEffect(getPlayerPosition(cid), CONST_ME_FLAMEAREA)
-end
-end  
+
+	if param == "" and doPlayerRemoveMoney(cid,config.price) then
+
+		doPlayerAddItem(cid, config.id)
+
+		doSendMagicEffect(getThingPos(cid), config.effect)
+
+	else
+
+		local config2 = {
+
+			error = "You do not have enought money.",
+
+			msg = "You bought an amulet of loss."
+
+		}
+
+		if doPlayerRemoveMoney(cid, config.price) then
+
+			doSendMagicEffect(getThingPos(cid), config.effect)
+
+			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, config2.msg)
+
+				for i = 1,param do
+
+					doPlayerAddItem(cid, config.id, 1)
+
+				end
+
+		else
+
+			doPlayerSendCancel(cid, config2.error)
+
+		end
+
+	end
+
+	return true
+	end
