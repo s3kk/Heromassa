7ebbd9e0e6fc37114de4f7cbb540cfb86ebed026
Sekk <kicknclash@gmail.com>
2016-10-23 19:40:56 -0200

    .

--- data/globalevents/scripts/broadcast.lua
+++ data/globalevents/scripts/broadcast.lua
@@ -4,6 +4,7 @@ CONFIG = {
     [1] = {message = "Quest Supreme Items no centro do templo de Sphere!", color = 22},
     [2] = {message = "Lottery System a cada 2 horas.", color = 22},
     [3] = {message = "Bem-Vindos ao Server, mapa HeroMassa", color = 22},
+	[4] = {message = "Use !notice para ver os comandos e informações interessantes do server!"}
 }
 
 function onThink()
