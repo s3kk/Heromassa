8d3948eebfd74771904ad0c43174dc57dcd45023
Sekk <kicknclash@gmail.com>
2017-01-18 00:44:53 -0200

    .

--- data/talkactions/scripts/mana.lua
+++
@@ -1,7 +0,0 @@
-function onSay(cid, words, param)
-
-local health = getCreatureMaxHealth(cid)
-
-doPlayerSendTextMessage(cid,MESSAGE_STATUS_CONSOLE_ORANGE, "Voce tem "..health.." health points.")
-return TRUE
-end
--- data/talkactions/scripts/vida.lua
+++
@@ -1,7 +0,0 @@
-function onSay(cid, words, param)
-
-local mana = getCreatureMaxMana(cid)
-
-doPlayerSendTextMessage(cid,MESSAGE_STATUS_CONSOLE_BLUE, "Voce tem "..mana.." mana points.")
-return TRUE
-end
